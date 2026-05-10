bba7b1b9028ea5d06be8f06ac664af407d42a778
danykennel-stack <danykennel@gmail.com>
2026-05-10 13:53:55 +0200

    Update prisma.ts

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = global;
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+module.exports = { prisma };
